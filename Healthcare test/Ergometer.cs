42ea7dd2a5d2195d6f5f16049ea64b837d074fc3
DionBartelen <dionbartelen@gmail.com>
2017-09-04 09:42:08 +0200

    Added getters and setters

--- Healthcare test/Ergometer.cs
+++ Healthcare test/Ergometer.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Healthcare_test
 {
-    class Class1
+    abstract class Ergometer
     {
+        abstract public ErgometerData getData();
+
+        abstract public void setRpm(int rpm);
+        abstract public void setSpeed(int speed);
+        abstract public void setDistance(int distance);
+        abstract public void setPulse(int pulse);
+        abstract public void setTime();
+        abstract public void setEnergy();
+        abstract public void setPower();
     }
 }
--- Healthcare test/ErgometerData.cs
+++ Healthcare test/ErgometerData.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Healthcare_test
 {
-    class Class1
+    class ErgometerData
     {
+        public int rpm { get; set; }
+        public int speed { get; set; }
+        public int distance { get; set; }
+        public int pulse { get; set; }
+        public int time { get; set; }
+        public int energy { get; set; }
+        public int power { get; set; }
     }
 }
