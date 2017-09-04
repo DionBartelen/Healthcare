473138646918f453d5d63f26548db29c19b8b57c
DionBartelen <dionbartelen@gmail.com>
2017-09-04 10:20:11 +0200

    Fixed the .csproj and renamed Form1 to GUIconnector

---
+++ Healthcare test/Ergometer/ErgometerCOM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Healthcare_test
+{
+
+    public class ErgometerCOM : Ergometer
+    {
+        public ErgometerCOM(string comport)
+        {
+
+        }
+
+
+    }
+}
